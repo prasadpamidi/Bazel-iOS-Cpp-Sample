fa067b45bbf6100a30a2c7c46c26a4524513a8d7
Prasad Pamidi <prasad.pamidi01@gmail.com>
2024-06-18 09:45:32 -0700

    Initial Commit

---
+++ BazelSample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BazelSample
+//
+//  Created by Prasad Pamidi on 6/18/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
